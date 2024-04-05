d2ebd04b29b846383e78b04b615cb7e7dea9f9ea
IllyaShparuk <illya.shparuk@knu.ua>
2024-04-05 09:22:11 +0300

    File comparator

--- project/main.py
+++ project/main.py
@@ -7,3 +7,12 @@ def files_reader(file_path) -> list[str]:
 def files_writer(file_path, lines):
     with open(file_path, 'w') as file:
         file.writelines(lines)
+
+
+def file_сomparator(file1_lines, file2_lines):
+    common_lines = list(set(file1_lines).intersection(file2_lines))
+
+    file1_unique = [line for line in file1_lines if line not in common_lines]
+    file2_unique = [line for line in file2_lines if line not in common_lines]
+
+    return common_lines, file1_unique, file2_unique
