c1d62aa4f37b9191ccdba0812bd42a0e2305f9d2
IllyaShparuk <illya.shparuk@knu.ua>
2024-04-05 09:13:59 +0300

    File writer

--- project/main.py
+++ project/main.py
@@ -2,3 +2,8 @@ def files_reader(file_path) -> list[str]:
     with open(file_path, 'r') as file:
         content = file.readlines()
     return content
+
+
+def files_writer(file_path, lines):
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
