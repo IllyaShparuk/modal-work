d65009ad7478333d3b5b5e4f86890bce67d32abe
IllyaShparuk <illya.shparuk@knu.ua>
2024-04-05 09:38:06 +0300

    File diff function

--- project/main.py
+++ project/main.py
@@ -1,11 +1,14 @@
+import os.path
+
+
 def files_reader(file_path) -> list[str]:
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         content = file.readlines()
     return content
 
 
 def files_writer(file_path, lines):
-    with open(file_path, 'w') as file:
+    with open(file_path, "w") as file:
         file.writelines(lines)
 
 
@@ -16,3 +19,13 @@ def file_сomparator(file1_lines, file2_lines):
     file2_unique = [line for line in file2_lines if line not in common_lines]
 
     return common_lines, file1_unique, file2_unique
+
+
+def text_diff(file1_path, file2_path):
+    file1_content_lines = files_reader(file1_path)
+    file2_content_lines = files_reader(file2_path)
+
+    common_lines, file1_unique, file2_unique = file_сomparator(file1_content_lines, file2_content_lines)
+
+    files_writer(os.path.join("..", "results", "same.txt"), common_lines)
+    files_writer(os.path.join("..", "results", "diff.txt"), file1_unique + file2_unique)
