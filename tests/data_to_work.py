6dcdff9a27ecde0a229e266f5617d7dd28fcbcef
IllyaShparuk <illya.shparuk@knu.ua>
2024-04-05 10:06:05 +0300

    Update fixture

--- tests/data_to_work.py
+++ tests/data_to_work.py
@@ -1,25 +1,29 @@
-import os.path
-
+import os
 import pytest
 from project.main import files_writer
 
 
 @pytest.fixture
 def real_data_fixture():
-    test_path1 = os.path.join("..", "files_to_compare", "test_file1.txt")
-    test_path2 = os.path.join("..", "files_to_compare", "test_file2.txt")
+    file1 = os.path.join("..", "files_to_compare", "test_file1.txt")
+    file2 = os.path.join("..", "files_to_compare", "test_file2.txt")
 
-    lines1 = [
-        "First line"
-        "Second line"
-        "Third line"
-    ]
-    lines2 = [
-        "First line"
-        "123"
-        "Third Line"
-        "Second line"
-    ]
+    files_writer(file1, [
+        "Line 1\n",
+        "Line 2\n",
+        "Line 3\n",
+        "Common line\n",
+        "Unique line 1\n",
+    ])
+    files_writer(file2, [
+        "Line 1\n",
+        "Line 2\n",
+        "Common line\n",
+        "Unique line 2\n",
+        "Unique line 3\n",
+    ])
 
-    files_writer(test_path1, lines1)
-    files_writer(test_path2, lines2)
+    yield file1, file2
+
+    os.remove(file1)
+    os.remove(file2)
